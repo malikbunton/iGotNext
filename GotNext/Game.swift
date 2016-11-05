64ff6af3e3bf961b96cbc912b34487a28974f3f4
malikbunton <malikbunton@gmail.com>
2016-11-05 16:10:11 -0400

    commit project

---
+++ GotNext/Game.swift
@@ -0,0 +1,13 @@
+//
+//  Game.swift
+//  GotNext
+//
+//  Created by Malik Bunton on 11/5/16.
+//  Copyright © 2016 SpellHouseTeam. All rights reserved.
+//
+
+import Foundation
+
+class Game: AnyObject {
+    
+}
---
+++ GotNext/Player.swift
@@ -0,0 +1,16 @@
+//
+//  Player.swift
+//  GotNext
+//
+//  Created by Malik Bunton on 11/5/16.
+//  Copyright © 2016 SpellHouseTeam. All rights reserved.
+//
+
+import Foundation
+
+class Player {
+    var <#name#> = <#value#>
+    
+    
+    
+}
